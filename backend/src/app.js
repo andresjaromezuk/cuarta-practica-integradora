d93c047f74961cbbde89c6ad5192ec951a6edf9e
andresjaromezuk <andres.jaromezuk@gmail.com>
2024-02-05 08:55:12 -0300

    adding mock

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,31 @@
 import express, { json, urlencoded } from 'express'
 import methodOverride from 'method-override' 
+
+//Routers
 import {apiRouter} from './router/api/apiRouter.js'
+import { mockRouter } from './router/mock/mock.product.router.js'
 import { webRouter } from './router/web/web.router.js'
+
+//DB
+import { connectDB } from './database/mongodb.js'
+
+//Views
 import {engine} from 'express-handlebars'
+
+//Authentication
 import { sessions } from './middleware/sessions.js'
 import { authenticate } from './middleware/authentication.js'
 import { cookies } from './middleware/cookies.js'
+
+//Websockets
 import {Server} from 'socket.io'
-import { connectDB } from './database/mongodb.js'
+
+//API
+import cors from 'cors'
+
+//Utils
 import path from 'path'
 import __dirname from './util.js'
-import cors from 'cors'
 
 //MongoDB
 await connectDB()
@@ -18,6 +33,8 @@ await connectDB()
 //Express
 import {PORT} from './config/server.config.js'
 const app = express()
+
+//API
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 
 //Middlewares 
@@ -27,17 +44,19 @@ app.use(urlencoded({ extended: true }))
 app.use(json())
 app.use(methodOverride('_method'))
 
+//Authentication
 app.use(sessions)
 app.use(cookies)
+app.use(authenticate)
 
+//Views
 app.engine('handlebars', engine())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'handlebars')
 
-app.use(authenticate)
-
 const server = app.listen(PORT, ()=>{console.log(`Servidor escuchando en puerto ${PORT}`)})
 
+//Websockets
 const ioServer = new Server(server)
 app.use((req,res,next) => {
   req['io'] = ioServer
@@ -47,3 +66,4 @@ app.use((req,res,next) => {
 //Rutas
 app.use('/api', apiRouter)
 app.use('/', webRouter)
+app.use('/mock', mockRouter)
